57bb41f757ccd9278d3175958dc0fd3fd6aac83d
utbuiquoc <utbuiquoc@gmail.com>
2024-09-28 23:46:22 +0700

    Install Filament, add artical tables

---
+++ database/migrations/2024_09_28_164249_create_article_tables.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->longText('description')->nullable();
+            $table->boolean('is_visible')->default(false);
+            $table->timestamps();
+        });
+
+        Schema::create('authors', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained(
+                table: 'users', indexName: 'id'   
+            )->cascadeOnDelete();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('photo')->nullable();
+            $table->longText('bio')->nullable();
+            $table->timestamps();
+        });
+
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('author_id')->nullable()->constrained()->cascadeOnDelete();
+            $table->foreignId('category_id')->nullable()->constrained()->nullOnDelete();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('excerpt')->nullable();
+            $table->string('banner')->nullable();
+            $table->longText('content');
+            $table->date('published_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+        Schema::dropIfExists('categories');
+        Schema::dropIfExists('authors');
+    }
+};
